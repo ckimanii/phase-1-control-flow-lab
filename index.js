faeba802a46feb3d06cf16146b88c7ea2bed7c8c
Kimani <alex.kimani@student.moringaschool.com>
2024-01-15 11:34:54 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(price){
   // Write your code here!
+  if (price <=400){
+    return 'This one is on me!'
+  }
+  else if (price <=2499){
+    return  'I will gladly take your thirty bucks.'
+  }
+  else (price >2500)
+    return 'No can do.'
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+console.log(scuberGreetingForFeet());
 
-function switchOnCharmFromTip(){
+  
+
+
+
+function ternaryCheckCity(city){
   // Write your code here!
-}
+  if (city === "NYC"){
+    return 'Ok, sounds good.'
+  }
+  else (city !== "NYC")
+    return 'No go.'
+  
+  }
+
+
+console.log(ternaryCheckCity())
+
+
+
+
+
+function switchOnCharmFromTip(trip){
+  // Write your code here!
+  if (trip === "generous"){
+    return 'Thank you so much.'
+  }
+  else if (trip === "not as generous"){
+    return 'Thank you.'
+  }
+  else (trip !== "any thing else")
+    return 'Bye.'
+  }
